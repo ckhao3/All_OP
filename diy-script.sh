6a603d76d94e2d9836a11c06518e816f56c6fb9a
ckhao <97717542+ckhao3@users.noreply.github.com>
2025-04-02 09:54:05 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -76,7 +76,7 @@ sed -i "s|ARMv8|ARMv8_PLUS|g" package/luci-app-amlogic/root/etc/config/amlogic
 #git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
 
 # Alist
-#git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
+git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
 
 # DDNS.to
 #git_sparse_clone main https://github.com/linkease/nas-packages-luci luci/luci-app-ddnsto
